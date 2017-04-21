3a910699b5551a05811ba7a273b45c4fcd48d3c7
Arjen Janssen <arjenjanssen@gmail.com>
2017-04-21 19:59:50 +0200

    search bar

---
+++ _site/assets/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+var tipuesearch = {"pages": [
+
+  
+  
+  
+  {
+    "title": "Bloei en Leef",
+    "text": "Klik hier om naar Bloei en Leef te gaan",
+    "tags": "",
+    "url": "/drafts/bloei-en-leef/"
+  },
+
+  
+  
+  
+  {
+    "title": "blogs",
+    "text": "Succesverhalen van gewone mensen",
+    "tags": "",
+    "url": "/blogs/"
+  },
+
+  
+  
+  
+  {
+    "title": "recepten",
+    "text": "prep-time cook-time ontbijt tussendoortje lunch diner side-dish toetje left-overs Avocadeau in één minuutje Gemberkoek van een GINGER kid Tarte tatin van Lucien Zoete aardappel BBQ Avocadeau in één minuutje HABA HABA lekker en super snel 1 min 0 min Gemberkoek van een GINGER kid Sweet and spicy, gettin' Feisty 15 min 30 min Tarte tatin van Lucien Welcome to the flip side of things. It Flipperdy Flaps your tongue! 20 min 30 min Zoete aardappel BBQ Juicy Lucy Smokin’ Sweetness BOMB 3 min 10-30 min Meer over Mizzi Mizzi, Miz, Mizzle, Mizu, Pummeke, Snuppa, Pretsel, Prammenbeest, Sjantie Kaliba, Loefoe, Pollanrossie, Tùmá, Djiema Djiema, Tuffie, Papadufulus, Poplapoemis, Poefie, Moksa, Poepandroe, Tummie Pops, Rodondendraak. Ja, naar deze namen luister ik allemaal. Heel vreemd dit. Miz zullen we dat ene eten wat je laatst had gemaakt? Je weet wel, dat wat iedereen zo lekker vond? Maar natuurlijk schattebillie, zeg ik dan. Ik weet alleen niet meer hoe en wat, was het nou met zus of zo, of toch niet. Met andere woorden. Ik wil volmondig JA zeggen op die vraag, maar ik heb dus eigenlijk geen idee wat ik heb gedaan om die tongstrelende neusprikkelende hammie jammie creatie te maken. Ik ben van het soort dat altijd iets anders probeert, er van houdt om te kijken wat er gebeurt, te kijken waar het schip strand, het fantastisch vindt om niet naar de supermarkt te gaan (omdat ik daar meestal gewoonweg geen zin in heb) en toch gewoon, met wat ik toevallig nog heb liggen, een masterlijke maaltijd neerzet die je vol overgave op wil peuzelen, wil verorberen, delicaat naar binnen wil snuffelen, op wil gaffelen, bunkeren, fretten. Kablam lekker happen man! Ik word ook wel de koningin van de hoppa-ploppa-food-show-stoppa genoemd. Gnagnagna, even zonder gekheid, ik kan dus schijnbaar van zo ongeveer niets, iets maken, van left-overs een volwaardige maaltijd, een feestje op tafel zetten, mondjes laten wateren, oogjes laten ploppen, harten laten verlangen naar meer, méér van dit lekkers... En... Daarna vergeten wat ik zo ongeveer heb gedaan om vervolgens weer iets anders te creëren, waar ik weer niet van zal leren. Dit. Ja, dit. Dit moet stoppen. Mizzi, zullen we dat ene eten wat je laatst had gemaakt? Je weet wel, dat wat iedereen zo lekker vond? Maar natuurlijk schattebillie, zeg ik dan. Dan plop ik mijn blog aan, die er overigens fabulous uitziet, spiek eventjes wat het ‘ hoe en wat ‘ ook al weer was, zet een lekker muziekje aan, ga de keuken in en kom met eye poppin’, tongue floppin’, jaw droppin’ lekkers naar buiten. é voila ben ik nu alweer in Valhalla? Beste vrienden, familie, collega’s, kennissen, buren, stadgenoten, landlopers, wereldreizigers, liefdesverspreiders weet je wat een fijne bijkomstigheid is van dit alles? Nou ja, je raadt het al. Dit spiekbriefje is niet alleen voor mij, dat zou hebberig zijn. En weet je, hebberigheid is niet echt mijn ding, ik ben meer van het delen, het samen spelen, het genieten met velen, van het extatisch dansen, met de lekkere dingen des levens sjansen. Dus kom, het is ook voor jou, join me, enjoy yourself, get your groove on, be free! Contact",
+    "tags": "",
+    "url": "/"
+  },
+
+  
+  
+  
+  {
+    "title": "layout sheet",
+    "text": "Dit is h1 Dit is h2 Dit is h3 Dit is h4 Dit is h5 Dit is h6 Dit is p dit is a Dit is li dit is ul Koken met Mizzi",
+    "tags": "",
+    "url": "/layoutSheet/"
+  },
+
+  
+  
+  
+  {
+    "title": "Avocadeau in één minuutje",
+    "text": "Dit cadeautje is altijd goed. Het is namelijk HABA HABA lekker en super snel. In één minuut heb je een heerlijk ontbijtje, tussendoortje of side dish. Ingredienten voor 2 personen 1 avocado 2 el olijfolie 2 tl soyasaus Beetje citroensap Zo maak je het: Snij de rijpe avocado doormidden. Hak met je mes in de pit en draai. Zo hop je de pit er zo uit. Hoppá! Nu heb je in beide helften een schattig kuiltje waar je eerst je lekkerste olijfolie in giet, dan een paar druppels zoute soyasaus in laat vallen en om het af te maken spriets je er een beetje citroensap bij. Je pakt een klein lepeltje waarmee je deze heerlijke smaaksensatie oplepelt, ja gewoon vanuit de avocado direct in je mond. Je kan dit solo eten. Maar het is ook lekker bij je ontbijtje desembrood met een gepocheerde eitje, je tosti lunch:, of gewoon je broodje kaas. Verzin het maar! Alles wordt er lekkerder van. HABA HABA.",
+    "tags": "",
+    "url": "/recepten/Avocadeau/"
+  },
+
+  
+  
+  
+  {
+    "title": "Gemberkoek van een GINGER kid",
+    "text": "JE MOEDER! Ja. Daar kan je nog eens wat van leren. Deze gemberkoek maakt mijn moeder namelijk al vele jaren, en haar GINGER kid (ik dus) heeft haar lekkere baksel opgepimpt tot in de 7de hemel. En, weet je wat, je kan deze koek eten als ontbijt, tussendoortje of gewoon de hele dag door als je daar zin in hebt want het is gezond. Daar houden we toch van tegenwoordig? Ingredienten voor 2 personen Groot stuk verse gember Rozijnen Havervlokken Meel Bakpoeder Eieren Zonnebloemolie Melk (koe, soya, haver, geit, amandel, whatever floats your boat) Kaneel Zout Sesamzaadjes Honing Zo maak je het: Zet de oven aan op 180 graden. Snij de gember in kleine stukjes (dit is het meeste werk). Pak een grote kom. Gooi de gember, rozijnen, havervlokken, meel, bakpoeder, eieren, zonnebloemolie, melk, kaneel, zout en de helft van de honing in de kom en roer dit met een garde door elkaar tot het goed gemixt is. Pak een bakvorm (ik gebruik een ronde met een diameter van +-23 cm en met van die schattige golfjes aan de buitenkant). Giet er een beetje zonnebloemolie in en smeer het door de hele bakvorm. Bedek de hele bodem met sesamzaadjes en giet je mengsel er in. Giet de andere helft van de honing over de bovenkant en zet het in de oven. Ga een half uurtje chillen met je billen en geniet van de heerlijke geur die het baksel door je kamer verspreidt. Even af laten koelen en hoppa smullen maar. Reken maar dat jij de volgende dag van je breakfast quickie geniet en daarna van je tussendoortje en misschien maak je er ‘s avonds wel een toetje van door er lekkere whipped cream bij te kwakken. JAMMIE! Genieten is toch ook gezond?",
+    "tags": "",
+    "url": "/recepten/gemberkoek-van-een-ginger-kid/"
+  },
+
+  
+  
+  
+  {
+    "title": "Tarte tatin van Lucien",
+    "text": "God mag weten wie Lucien is. Wat ik wel weet is dat deze tarte tatin je tong laat flapperen. Niet normaal lekker dit. Hij is knapperig, zacht, zoet en zout tegelijk en toch best makkelijk te versieren. Ik dacht dat tarte tatins maken niet te doen was. Wel dus. Easy peasy, je snijdt, bakt, bedekt en de oven doet de rest. En ja, dan het spannende onderdeel, omdraaien maar! Welcome to the flip side of things! Ingredienten voor 2 personen 500 gram pompoen 200 gram sjalotten 100 gram zachte geitenkaas 1 klein handje salie Flinke scheut ammaretto Flinke klont roomboter Zout naar smaak Peper naar smaak 4 plakjes bladerdeeg Zo maak je het: Zet de oven aan op 180 graden. Leg het bladerdeeg op het aanrecht om te ontdooien. Snij de pompoen in grove stukken. Snij de sjalotten in grove parten. Zet een koekenpan, die ook in de oven mag, op het vuur. Doe er lekker veel boter in. Bak de pompoen 10 minuten en doe dan de sjalotten met de salie erbij. Bak 5 minuten mee. Giet er een flinke scheut ammaretto over heen en ruik even, deze geur is heerlijk! Voeg zout en peper toe. Verdeel de pompoen en sjalotten gelijkmatig in de pan en leg de stukken geitenkaas er tussen. Bedek je baksel met het bladerdeeg. Zet de pan een half uur in de oven, tot de bovenkant goudbruin is. Leg een plank of bord, waar je de tarte tartin op wil serveren, op de pan en draai ze samen om. Welcome to the flip side of things. It Flipperdy Flaps your tongue!",
+    "tags": "",
+    "url": "/recepten/tarte-tatin-van-lucien/"
+  },
+
+  
+  
+  
+  {
+    "title": "Zoete aardappel BBQ",
+    "text": "Stel je voor hè, je bent heerlijk aan het BBQ-en in de zon. Je hebt allerlei lekkers op het rooster geknald en in de kolen ligt een zoete aardappel een Juicy Lucy Smokin’ Sweetness BOMB te worden tussen de hete kolen. Die je kan eten bij alles wat je hebt gemaakt, je vlees, je vis, je gegrilde groenten, en knapperige broodjes. NOMNOMNOM. Ingredienten voor 2 personen 1 grote zoete aardappel 0.5 rode verse chilipeper Beetje limoensap 1 el olijfolie Zout Zo maak je het: Pak de zoete aardappel(len) in met aluminiumfolie (glimmende kant naar buiten) en stop ze tussen de hete kolen in je BBQ. Ga heerlijk genieten van je muziekje, je wijntje en je gezelschap, je BBQ doet het werk. Snij ondertussen het chilli pepertje in kleine stukjes. Om de gaarheid te checken prik je na een tijdje met een vork of iets anders prikkerigs zoals een satéprikker in de zoete aardappel. Als dit makkelijk gaat dan is je Juicy Lucy Smokin’ Sweetness BOMB ready to serve! Bij de kleintjes zal dit 10 minuutjes duren en bij groten een half uurtje. Prik dus gewoon voor de zekerheid. De buitenkant kan helemaal verkolen, schrik niet! De binnenkant wordt hoe dan ook awesome! Snij je Juicy Lucy Smokin’ Sweetness BOMB doormidden en lepel de goddelijke inhoud er uit. Leg het op een houten plank of in een schaal, gooi de chillipeper er over heen, knijp er een halve limoen boven uit, drizzle er wat olijfolie overheen en draai er wat zout op. Eet deze smokin’ BOMB bij alles wat je ook eet, een beetje op een knapperig stokbroodje, bij je vlees, vis, groenten, mix and match away!",
+    "tags": "",
+    "url": "/recepten/zoete-aardappel-bbq/"
+  }
+
+]};
