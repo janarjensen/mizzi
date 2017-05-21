4d521e63155ff1283b2c87da3026837ec68ad9c9
Arjen Janssen <arjenjanssen@gmail.com>
2017-05-21 19:53:23 +0200

    3 extra gerechten

---
+++ _recepten/pizzaRapido.md
@@ -0,0 +1,49 @@
+---
+layout: taart
+title: Pizza deliziosa rapidamente
+imageName: pizzaSalami.jpg
+cuttingBoard: pizza.png
+dateAdded: 21 Mei 2017
+intro: Homemade pizza in 10 miuten
+prepTime: 5
+cookTime: 5
+ontbijt: false
+tussendoortje: false
+lunch: true
+diner: true
+sideDish: false
+toetje: false
+leftOvers: true
+---
+
+Deze pizza’s zijn lekker en lekker makkelijk. In een handomdraai ben je aan het smikkelen. Je eet je vingers er bij op. Dit is namelijk echt fingerfood!
+
+##### Ingrediënten voor 1 pizza
+##### Basis
+* 1 tortilla vel
+* 4 el pastasaus
+* knoflook naar smaak
+* geraspte kaas
+
+##### Topping ideeën
+**pizza salami**
+* 5 plakjes salami
+* 4 cherry tomaatjes gehalveerd
+* handje verse basilicum
+
+![pizza salami][pizzaSalami]
+
+
+**pizza serano**
+* 3 plakken serano ham
+* 4 cherry tomaatjes gehalveerd
+* handje rucola
+* 5 dotjes rode pesto
+
+![pizza serano][pizzaSerano]
+
+##### Zo maak je het:
+Verwarm de oven voor op 220 graden. Pak een tortilla en smeer er 4 el pastasaus op, doe er wat gehakte knoflook op en strooi er geraspte kaas overheen. Dan komt de topping; wat zal ik er eens op doen?. Mijn pizza-ding is ‘keep it simple’, maar je kan ook helemaal losgaan of je kan je leftovers er op knallen. Do your thing! Hoppatee. 5 minuten in de oven en smullen maar.
+
+[pizzaSalami]: {{ site.baseurl }}../../img/recepten/pizzaSalami.jpg
+[pizzaSerano]: {{ site.baseurl }}../../img/recepten/pizzaSerano.jpg
--- _site/assets/tipuesearch/tipuesearch_set.js
+++ _site/assets/tipuesearch/tipuesearch_set.js
@@ -12,23 +12,24 @@ Stop words
 Stop words list from http://www.ranks.nl/stopwords
 */
 
-var tipuesearch_stop_words = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
+var tipuesearch_stop_words = ["Een","over","boven","na","weer","tegen","allen","als","bij","zijn","voor","zijn","onder","tussen","beide","maar","Niet","niet","kan","beneden","onder","elk","weinig","voor","van","verder","had","heeft","heb","hebben","hij","zal","hier","Haar","haar","hem","zelf","zijn","hoe","ik","zou","heb","als","in","is","het","zelf","Meer","meest","mijn","mezelf","nee","noch","van","uit","op","eenmaal","Alleen","of","over","eigen","zelfde","ons","zij","moet","zo","sommigen","dan","dat","Dat","de","hun","hen","zelf","daar","deze","dit","die","Tot","tot","we","waren","wat","wanneer","waar","terwijl","wie","waarom","met","Jij","je","jouwe","jezelf"];
 
 
 // Word replace
 
 var tipuesearch_replace = {'words': [
-     {'word': 'tip', 'replace_with': 'tipue'},
-     
+     {'word': 'mizzy', 'replace_with': 'Mizzi'},
+     {'word': 'missy', 'replace_with': 'Mizzi'},
+     {'word': 'missie', 'replace_with': 'Mizzi'},
+     {'word': 'mizzi', 'replace_with': 'Mizzi'},
 ]};
 
 
 // Weighting
 
 var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 20},
-     {'url': 'http://www.tipue.com/search', 'score': 30},
-     {'url': 'http://www.tipue.com/is', 'score': 10}
+     {'url': '{{ baseurl }}/recepten', 'score': 30},
+     {'url': '{{ baseurl }}', 'score': 20},
 ]};
 
 
@@ -45,9 +46,7 @@ var tipuesearch_stem = {'words': [
 
 var tipuesearch_related = {'searches': [
      {'search': 'tipue', 'related': 'Tipue Search'},
-     {'search': 'tipue', 'before': 'Tipue Search', 'related': 'Getting Started'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'jQuery'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'Blog'}
+
 ]};
 
 
