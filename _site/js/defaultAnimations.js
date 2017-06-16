e7f8285d1ff7ed5008d51989c6b00ac4cd395357
Arjen Janssen <arjenjanssen@gmail.com>
2017-06-16 12:27:27 +0200

    Keeps links in iOS in app

--- _site/js/defaultAnimations.js
+++ _site/js/defaultAnimations.js
@@ -1,3 +1,7 @@
+// Keeps links in iOS in app
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+
 // set equal heights to all recepes
 $(function() {
     $('.receptCard').matchHeight();
